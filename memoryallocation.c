d67cd8679dbb9a8cf399c775c275bb7cdf124b27
jayz37 <jayvora1612002@gmail.com>
2022-07-17 19:49:38 -0400

    jay vora commit

---
+++ memoryallocation.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <semaphore.h>
+#include <stdbool.h>
